050244de017c558f7d019a986226851de90664f7
lucasgfaj <lucasgfajardo09@gmail.com>
2023-11-30 22:36:57 -0300

    Att

--- starwarsjson.sh
+++ starwarsjson.sh
@@ -9,24 +9,20 @@ mkdir "$diretorio"
 #Confirmando a criação
 echo "Diretório '$diretorio' criado com sucesso"
 
-#Entrando no diretório
-cd $diretorio
-
 #Copiando conteúdo JSON
-curl -o $arquivo.json https://swapi.dev/api/people
+wget https://swapi.dev/api/people
+mv people $diretorio
 
 #Informando um termo a ser pesquisado
 read -p "Informe um termo para ser pesquisar no arquivo: " termo
 
-jq [.] people
-
-grep -i "$termo" "$diretorio/$arquivo.json" >>
-
-
-#Extraindo Linhas para output.txt
+#Extraindo linhas para output.txt
+jq [.] $diretorio/people | grep -i $termo  >> $diretorio/output.txt
+cat $diretorio/output.txt
 
 #Buscando seção (objeto) JSON
-
+read -p "Informe uma seção que você deseja ver: " secao
+jq '.$secao' people
 #Extraindo a informação da seção buscada JQ
 
 #Pergutando ao usuário qual valor ele gostaria de ver

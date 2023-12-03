53d3773ea13dfed7fc4b1031a5651d19421709a4
lucasgfaj <lucasgfaj@gmail.com>
2023-12-03 18:21:37 -0300

    ShellScript

--- starwarsjson.sh
+++ starwarsjson.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 
-#Perguntando nome para o diretório
+#1. Perguntando nome para o diretório
 read -p "Informe um nome para seu diretório: " diretorio
 
-#Criando diretório
+#1.1. Criando diretório
 mkdir "$diretorio"
 
-#Confirmando a criação
+#1.2. Confirmando a criação
 echo "Diretório '$diretorio' criado com sucesso"
 
-#Copiando conteúdo JSON
+#2. Copiando conteúdo JSON
 wget https://swapi.dev/api/people
 mv people $diretorio
 
-#Informando um termo a ser pesquisado
+#3. Informando um termo a ser pesquisado
 read -p "Informe um termo para ser pesquisar no arquivo: " termo
 
-#Extraindo linhas para output.txt
+#4. Extraindo linhas para output.txt
 jq [.] $diretorio/people | grep -i $termo  >> $diretorio/output.txt
 cat $diretorio/output.txt
 
-#Buscando seção (objeto) JSON
+#5. Buscando seção (objeto) JSON
 read -p "Informe uma seção que você deseja ver: " secao
 
-jq [.$secao] $diretorio/people >> $diretorio/output.txt
+#6. Extraindo a informação da seção buscada JQ
+jq ".results[$secao]" $diretorio/people >> $diretorio/output.txt
 cat $diretorio/output.txt
 
-#Extraindo a informação da seção buscada JQ
+#7. Perguntar ao usuário qual valor isolado desta seção ele gostaria de ver
+read -p "Informe um valor isolado desta seção: " valor
 
-#Pergutando ao usuário qual valor ele gostaria de ver
+#8. Exibir o valor isolado na tela
+echo "O valor isolado escolhido é: $valor"
 
-#Exibindo o valor na tela
+#9. Criando um novo arquivo JSON com o conteúdo desta seção totalmente alterada
+jq ".results[$secao].$termo = \"$valor\"" $diretorio/people > $diretorio/alterado_arquivo.json
+echo "Novo arquivo JSON criado com sucesso: $diretorio/alterado_arquivo.json"
 
-#Criando um novo arquivo JSON com o conteúdo desta seção totalmente alterada
+#10. Criar 5 cópias deste novo arquivo
+for i in {1..5}
+do
+    cp "$diretorio/alterado_arquivo.json" "$diretorio/people_copia_alterada$i"
+done
 
-#Criar 5 cópias deste novo arquivo
+#10.1 Conclusão
+echo "Operações concluídas."
